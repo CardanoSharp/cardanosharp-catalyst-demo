f476ce3857ae162874604da61359b1c2d46ef56b
Kyle Johns <kjohns1337@gmail.com>
2021-08-02 23:05:55 -0400

    starting the CQRS pattern

---
+++ CardanoSharp.CatalystDemo/Commands/GenerateMnemonic.cs
@@ -0,0 +1,48 @@
+﻿using CardanoSharp.Wallet;
+using CardanoSharp.Wallet.Models;
+using CardanoSharp.Wallet.Models.Keys;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CardanoSharp.CatalystDemo.Commands
+{
+    public static class GenerateMnemonic
+    {
+        public class GenerateMnemonicCommand: IRequest<GenerateMnemonicResponse>
+        {
+            public int Size { get; private set; }
+
+            public GenerateMnemonicCommand(int size)            
+            {
+                Size = size;
+            }
+        }
+
+        public class GenerateMnemonicHandler : IRequestHandler<GenerateMnemonicCommand, GenerateMnemonicResponse>
+        {
+            private IKeyService _keyService;
+
+            public GenerateMnemonicHandler(IKeyService keyService)
+            {
+                _keyService = keyService;
+            }
+
+            public async Task<GenerateMnemonicResponse> Handle(GenerateMnemonicCommand request, CancellationToken cancellationToken)
+            {
+                return new GenerateMnemonicResponse()
+                {
+                    Mnemonic = _keyService.Generate(request.Size)
+                };
+            }
+        }
+
+        public class GenerateMnemonicResponse
+        {
+            public Mnemonic Mnemonic { get; set; }
+        }
+    }
+}
