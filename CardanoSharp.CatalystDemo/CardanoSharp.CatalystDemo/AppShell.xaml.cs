d8492c7758bd61f9c6f6cc1fe177355fadcc9c19
Kyle Johns <kjohns1337@gmail.com>
2021-08-06 16:16:59 -0400

    removed old project added new

---
+++ CardanoSharp.CatalystDemo/CardanoSharp.CatalystDemo/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using CardanoSharp.CatalystDemo.ViewModels;
+using CardanoSharp.CatalystDemo.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace CardanoSharp.CatalystDemo
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
