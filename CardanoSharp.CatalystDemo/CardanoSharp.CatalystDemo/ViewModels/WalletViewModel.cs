747f48255bfeed05fc63a036141eee33123469ad
Kyle Johns <kjohns1337@gmail.com>
2021-08-30 20:49:14 -0400

    i believe the backend is done

--- CardanoSharp.CatalystDemo/CardanoSharp.CatalystDemo/ViewModels/WalletViewModel.cs
+++ CardanoSharp.CatalystDemo/CardanoSharp.CatalystDemo/ViewModels/WalletViewModel.cs
@@ -1,4 +1,7 @@
-﻿using CardanoSharp.CatalystDemo.Services;
+﻿using CardanoSharp.CatalystDemo.Models;
+using CardanoSharp.CatalystDemo.Services;
+using CardanoSharp.Wallet.Models.Addresses;
+using CardanoSharp.Wallet.Models.Keys;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,9 +11,21 @@ namespace CardanoSharp.CatalystDemo.ViewModels
 {
     public class WalletViewModel : BaseViewModel
     {
-        private string mnemonicWords;
+        //This holds the Address we use to Send and Receive ADA
+        public Address Address { get; set; }
+        //This holds our Mnemonic for display
+        //note: never do this in a live app
+        public Mnemonic Mnemonic { get; set; }
+        //This is a list of UTXOs that below to our Address
+        public List<Utxo> Utxos { get; set; }
+        //This is the last transaction id
+        public string TransactionId { get; set; }
 
+        //This is the form object we use to send ADA with Metadata message
         public SendForm SendForm { get; set; }
+        //This is the form object we use to restore an existing wallet
+        public RestoreForm RestoreForm { get; set; }
+
 
         public Command GenerateWallet { get; }
         public Command RestoreWallet { get; }
@@ -34,21 +49,27 @@ namespace CardanoSharp.CatalystDemo.ViewModels
             _walletStore = walletStore;
         }
 
-        private async void OnGenerateWallet()
+        public async void OnGenerateWallet()
         {
-            var mnemonic = await _walletService.GenerateMnemonic(15);
-            _walletStore.SetAccountKeys(mnemonic);
+            Mnemonic = await _walletService.GenerateMnemonic(15);
+            SetupAccountNode();
         }
 
-        private async void OnRestoreWallet()
+        public async void OnRestoreWallet()
         {
-            var mnemonic = await _walletService.RestoreMnemonic("");
-            _walletStore.SetAccountKeys(mnemonic);
+            Mnemonic = await _walletService.RestoreMnemonic(RestoreForm.Words);
+            SetupAccountNode();
+        }
+
+        private async void SetupAccountNode()
+        {
+            _walletStore.SetAccountKeys(Mnemonic);
+            Address = await _walletService.GetAddress(_walletStore.AccountKeys, 0);
         }
 
         public async void OnRefreshUtxos()
         {
-            var utxos = _blockfrostService.GetUtxos("");
+            Utxos = await _blockfrostService.GetUtxos(Address.ToString());
         }
 
         public async void OnSubmitTx()
@@ -63,7 +84,8 @@ namespace CardanoSharp.CatalystDemo.ViewModels
                 Message = SendForm.Message
             };
 
-            var transaction = await _transactionService.Send(request, keyPair);
+            var response = await _transactionService.Send(request, keyPair);
+            TransactionId = response.TransactionHash;
         }
     }
 
@@ -73,5 +95,10 @@ namespace CardanoSharp.CatalystDemo.ViewModels
         public string RecieverAddress { get; set; }
         public decimal Amount { get; set; }
         public string Message { get; set; }
+    }
+
+    public class RestoreForm
+    { 
+        public string Words { get; set; }
     }
 }
